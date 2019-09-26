76cae726b1ccd55349af4e637d5aceb581f78335
Ania <mrowicka.ania@gmail.com>
2019-09-26 21:17:27 +0200

    form

---
+++ form.js
@@ -0,0 +1,9 @@
+function submitForm(){
+  let firstName = document.getElementById('FirstName').value;
+  let secondName = document.getElementById('secondName').value;
+  let email = document.getElementById('Email').value;
+  console.log(firstName);
+  console.log(secondName);
+  console.log(email);
+
+}
